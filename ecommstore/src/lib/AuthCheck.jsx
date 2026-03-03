0ec8341cbcbc3785a10eecd6d7186f5e7c219bf2
wa <wa@agiteks.com>
2026-03-03 19:57:57 +0500

    fix: remove unnecessary router logic from CheckAuth component to streamline authentication checks

--- ecommstore/src/lib/AuthCheck.jsx
+++ ecommstore/src/lib/AuthCheck.jsx
@@ -1,15 +1,9 @@
 "use client";
 import { useEffect } from "react";
 import useAuthStore from "@/store/authStore";
-import { useRouter } from "next/navigation";
 
 export default function CheckAuth({ children }) {
   const checkAuth = useAuthStore((s) => s.checkAuth);
-  const router = useRouter();
-  const { user, isAuthenticated } = useAuthStore();
-  if (!isAuthenticated) {
-    router.push("/");
-  }
 
   // Run auth check on mount
   useEffect(() => {
